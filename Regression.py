1a390a6285cac80bd13f535c41bf505e26b6d3a6
MalakRosas <malakrosass@gmail.com>
2024-03-09 20:47:49 +0200

    final

--- Regression.py
+++ Regression.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 
-file_path = "files/California_Houses.csv"
+file_path = "California_Houses.csv"
 data = pd.read_csv(file_path)  # data is a dataframe
 # Split the balanced dataset into training and the rest
 training_set, remaining_data = train_test_split(data, test_size=0.3, random_state=42)
@@ -30,12 +30,12 @@ linear_model.fit(x_train, y_train)
 # Evaluate Linear Regression on validation set( Calculate Mean Square Error for Linear Regression)
 linear_val_predictions = linear_model.predict(x_validation)
 linear_val_mse = mean_squared_error(y_validation, linear_val_predictions)
-print("*****************")
+print("***************************************************")
 print("Linear Regression Validation MSE:", linear_val_mse)
 # Calculate Mean Absolute Error for Linear Regression
 linear_val_mae = mean_absolute_error(y_validation, linear_val_predictions)
 print("Linear Regression Validation MAE:", linear_val_mae)
-print("*****************")
+print("***************************************************")
 
 # Apply Lasso Regression
 lasso_model = Lasso(alpha=0.1, max_iter=10000)  # You can adjust the alpha parameter
@@ -44,12 +44,12 @@ lasso_model.fit(x_train, y_train)
 # Evaluate Lasso Regression on validation set
 lasso_val_predictions = lasso_model.predict(x_validation)
 lasso_val_mse = mean_squared_error(y_validation, lasso_val_predictions)
-print("*****************")
+print("***************************************************")
 print("Lasso Regression Validation MSE:", lasso_val_mse)
 # Calculate Mean Absolute Error for Lasso Regression
 lasso_val_mae = mean_absolute_error(y_validation, lasso_val_predictions)
 print("Lasso Regression Validation MAE:", lasso_val_mae)
-print("*****************")
+print("***************************************************")
 
 # Apply Ridge Regression
 ridge_model = Ridge(alpha=1.0)
@@ -63,7 +63,7 @@ print("Ridge Regression Validation MSE:", ridge_val_mse)
 ridge_val_predictions = ridge_model.predict(x_validation)
 ridge_val_mae = mean_absolute_error(y_validation, ridge_val_predictions)
 print("Ridge Regression Validation MAE:", ridge_val_mae)
-print("*****************")
+print("***************************************************")
 
 # Calculate Mean Squared Error for Linear Regression on test set
 linear_test_predictions = linear_model.predict(x_test)
@@ -72,7 +72,7 @@ print("Linear Regression Test MSE:", linear_test_mse)
 # Calculate Mean Absolute Error for Linear Regression on test set
 linear_test_mae = mean_absolute_error(y_test, linear_test_predictions)
 print("Linear Regression Test MAE:", linear_test_mae)
-print("*****************")
+print("***************************************************")
 
 # After selecting the best model based on validation performance, you can evaluate it on the test set
 lasso_test_predictions = lasso_model.predict(x_test)
@@ -85,13 +85,13 @@ print("Lasso Regression Test MAE:", lasso_test_mae)
 # Calculate Mean Squared Error for Ridge Regression on test set
 ridge_test_predictions = ridge_model.predict(x_test)
 ridge_test_mse = mean_squared_error(y_test, ridge_test_predictions)
-print("*****************")
+print("***************************************************")
 
 print("Ridge Regression Test MSE:", ridge_test_mse)
 # Calculate Mean Absolute Error for Ridge Regression on test set
 ridge_test_mae = mean_absolute_error(y_test, ridge_test_predictions)
 print("Ridge Regression Test MAE:", ridge_test_mae)
-print("*****************")
+print("***************************************************")
 
 # Validation Set Evaluation
 # Find the best-performing model based on validation set MSE
@@ -110,7 +110,7 @@ else:
     print("MSE:", best_model_mse)
     print("MAE:", best_model_mae)
 
-print("*****************")
+print("***************************************************")
 
 # Test Set Evaluation
 # Evaluate the best-performing model on the test set
@@ -127,5 +127,5 @@ else:  # Ridge Regression
     print("MSE:", ridge_test_mse)
     print("MAE:", ridge_test_mae)
 
-print("*****************")
-print("*****************")
+print("***************************************************")
+print("***************************************************")
