385c0e692ce1da8170cb21f36dc521a877d8d879
MalakRosas <malakrosass@gmail.com>
2024-03-17 21:05:17 +0200

    last modification

--- Classification.py
+++ Classification.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import MinMaxScaler
+
 
 # Load the dataset
-file_path = "files\magic04.data"
+file_path = "magic04.data"
 col_names = ['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym', 'fM3Long', 'fM3Trans', 'fAlpha', 'fDist',
              'class']
 data = pd.read_csv(file_path, names=col_names)
@@ -50,33 +52,53 @@ k_values = range(1, 21)
 
 # Initialize lists to store accuracy scores
 accuracy_scores = []
-
+best_k = 0
+true_values = 0
+predicted_values = 0  # for best k
+max_accuracy = 0
+min_error = 100000
 # Iterate over different values of k
 for k in k_values:
     # Initialize the k-NN classifier with the current k
     knn_classifier = KNeighborsClassifier(n_neighbors=k)
-    
+
     # Train the classifier on the training data
     knn_classifier.fit(X_train, y_train)
-    
+
     # Predict the classes for validation data
     y_val_pred = knn_classifier.predict(X_val)
-    
     # Calculate accuracy score for the current k value
     accuracy = accuracy_score(y_val, y_val_pred)
-    
+
+    # Calculate error for the current k value
+    error = 1 - accuracy
+    if accuracy > max_accuracy:
+        max_accuracy = accuracy
+        best_k = k
+        true_values = y_val.values
+        predicted_values = y_val_pred
+        min_error = error
+
     # Append accuracy score to the list
     accuracy_scores.append(accuracy)
     conf_matrix = confusion_matrix(y_val, y_val_pred)
-    
+
     # Print evaluation metrics for the current k value
     print(f"k = {k}:")
     print("Accuracy:", accuracy)
+    print("Error:", error)
     print("Classification Report:")
     print(classification_report(y_val, y_val_pred))
     print("Confusion Matrix:")
     print(conf_matrix)
     print("-------------------------------------")
+print("Best k:", best_k)
+print("Maximum accuracy:", max_accuracy)
+print("Minimum error:",min_error)
+print("True labels for best k:")
+print(true_values)
+print("Predicted labels for best k:")
+print(predicted_values)
 
 # Plot accuracy scores for different values of k
 plt.figure(figsize=(10, 6))
